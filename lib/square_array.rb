33a3c81f4caec79045fb6ad1f83afbf138bee1cb
Isak Kallenbach <kallenbach13@gmail.com>
2020-01-24 18:34:16 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,8 +3,8 @@ def square_array(array)
   counter = 0
   
   while array[counter] do
-    array[counter] ** 2
+    new_array.push(array[counter] ** 2)
     counter += 1
-    array[counter]
   end
+  new_array
 end
